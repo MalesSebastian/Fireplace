35d3ac3acd0b66cc8e989defe11b0c29c2dfcf19
Senibelan <malessebastian@gmail.com>
2015-11-07 18:16:56 +0200

    Forms style

--- authen/forms.py
+++ authen/forms.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+#from django.forms import ModelForm
+from django import  forms
 
-class UserForm(ModelForm):
+class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'testcss'}),
+            'email': forms.TextInput(attrs={'class': 'create_acount'}),
+            'password': forms.TextInput(attrs={'class': 'create_acount'})
+        }
