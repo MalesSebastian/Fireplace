de24c28d8f8145b6889f60f682fd296dd36957c9
Malessebastian <malessebastian@gmailc.om>
2015-11-28 00:01:49 +0200

    new post template

--- new_post/urls.py
+++ new_post/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
-from django.views.generic import TemplateView
+from django.conf.urls import include, url
+from django.contrib import admin
+from . import views
 
 urlpatterns = [
-
+    url(r'^submit', views.submit, name='submit')
 ]
--- new_post/views.py
+++ new_post/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
+def submit(reqest):
+    return render_to_response('new_post/create_post.html')
