6bbc3cf9572a834f3eb835a0a52fce6d982d4fbd
Malessebastian <malessebastian@gmailc.om>
2015-11-27 23:01:05 +0200

    Post model

--- new_post/models.py
+++ new_post/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from django.utils import timezone
 
-class Post(models):
+class Post(models.Model):
     title = models.CharField(max_length=100)
     post_text = models.TextField()
     time = timezone.now()
+    upvote = models.IntegerField()
+    downvote = models.IntegerField()
 
-    def __unicode__(self):
-        return self.title
+    def set_title(self, title):
+        self.title = title
+
+    def set_text(self, text):
+        self.post_text = text
+
+    def get_upvote (self):
+        return self.upvote
+
+    def get_downvote (self):
+        return self.downvote
 # unfinished work
 
 
