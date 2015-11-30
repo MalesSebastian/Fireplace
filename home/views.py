205adfcbbe97900d87eb7c81fd514fff444d3e4c
Malessebastian <malessebastian@gmailc.om>
2015-11-30 14:14:16 +0200

    New_post final

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('home/Front.html')
+    if request.user.is_authenticated():
+        return render_to_response('home/Front.html')
+    #else
+        #alt template
 # Create your views here.
--- new_post/views.py
+++ new_post/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response
 from models import Post
 import pytz
 from django.utils import timezone
+from django.conf import settings
+from django.utils.timezone import activate
 
 def submit(request):
     if request.method == "GET":
@@ -11,9 +13,9 @@ def submit(request):
         user_ses = request.user
         if post_title is not None:
             new_post = Post(post_title, post_text, user_ses,category)
-            print new_post.get_title() , '\n'
-            print new_post.get_text() , '\n'
-            print new_post.get_user() , '\n'
-            print new_post.get_category() , '\n'
-            print new_post.get_time()
+           # print new_post.get_title() , '\n'
+           # print new_post.get_text() , '\n'
+           # print new_post.get_user() , '\n'
+           # print new_post.get_category() , '\n'
+           # print new_post.get_time()
     return render_to_response( 'new_post/create_post.html')
