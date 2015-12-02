1cd3876b5019fede02a0fcb70a50cbc270af30af
Malessebastian <malessebastian@gmailc.om>
2015-12-02 22:45:38 +0200

    CustomUserModel

--- authen/models.py
+++ authen/models.py
@@ -1,3 +1,26 @@
+""""
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserModel(User):
+    points = models.IntegerField(default=0)
+    badges = models.
+
+    def get_point(self):
+        return self.points
+
+    def get_badges(self):
+        return self.badges
+
+    def set_badges(self, badge):
+
+
+    def set_point(self, point):
+        self.points = self.points + point
+
+
+
+            Work in progress 
+"""
+
