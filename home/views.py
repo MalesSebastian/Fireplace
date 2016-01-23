c0f8a5038d86413669cb863754b2233bfcc8fca8
Sebastian <MalesSebastian>
2016-01-23 16:51:05 +0200

    Front page perfection

--- home/views.py
+++ home/views.py
@@ -4,7 +4,9 @@ from new_post.models import Post
 
 def home(request):
     if request.user.is_authenticated():
-        return render_to_response('home/Front.html', {'posts': Post.objects.order_by('-time')})
+        return render_to_response('home/Front.html', {
+            'posts': Post.objects.order_by('-time')
+        })
     # else
         # alt template
 # Create your views here.
--- new_post/models.py
+++ new_post/models.py
@@ -1,28 +1,23 @@
 from django.db import models
-from django.utils import timezone
 import uuid
 from django.core.urlresolvers import reverse
 
 
 class Post(models.Model):
 
-    title = models.CharField(max_length=100)
-    post_text = models.TextField()
-    #slug = models.SlugField(default=uuid.uuid1, unique=True)
+    title = models.CharField(max_length=100, default='')
+    post_text = models.TextField(default='')
+    slug = models.SlugField(default=uuid.uuid1, unique=True)
     time = models.DateField(auto_now_add=True, editable=False)
     up_vote = models.IntegerField(default=0)
     category = models.TextField(default='')
     submitter = models.TextField(default='')
 
-    def __init__(self, title, post_text, user, category):
-        self.title = title
-        self.post_text = post_text
-        self.time = timezone.now()
-        self.submitter = user
-        self.category = category
+    def __unicode__(self):
+        return self.title
 
-  #  def get_absolute_url(self):
-  #s      return reverse('post', args=[self.slug])
+    def get_absolute_url(self):
+        return reverse('post', args=[self.slug])
 
     def set_title(self, title):
         self.title = title
